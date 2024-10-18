fedf310c781ffe62f05896aecc64a1fcd02579a7
Makov259 <mak00v259@gmail.com>
2024-10-18 09:45:46 +0300

    new commit

--- src/layouts/SearchBooksPage/components/SearchBook.tsx
+++ src/layouts/SearchBooksPage/components/SearchBook.tsx
@@ -1,6 +1,5 @@
 import BookModel from "../../../models/BookModel";
 import React from "react";
-
 export const SearchBook: React.FC<{ book: BookModel }> = (props) => {
   return (
     <div className="card mt-3 shadow p-3 mb-3 bg-body rounded">
